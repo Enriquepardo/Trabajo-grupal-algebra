b30c44203a7bc693a8d004a55fb313188531ec40
Enriquepardo <epardser@myuax.com>
2023-06-01 20:04:00 +0200

    cambios barra navegacion

--- index.py
+++ index.py
@@ -1,9 +1,9 @@
 
 
-print('--Lo primero que hay que hace es activar el entorno virtual de Python. Para ello, en la consola de Python, escribe lo siguiente:')
+print('--> Lo primero que hay que hace es activar el entorno virtual de Python. Para ello, en la consola de Python, escribe lo siguiente:')
 print('virtualenv venv')
-print('--Ahora lo segundo que tienes que hacer es copiar y pegar lo siguiete en la consola de Python:')
-print('pip install -r ".\requirements.txt" ')
+print('--> Ahora lo segundo que tienes que hacer es instalar los requirements:')
+print(r'pip install -r ".\requirements.txt" ')
 
 if __name__ == 'main':
     pass
