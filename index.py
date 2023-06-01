33af7f71aa79847d111651bab36a38d12924ac2f
Enriquepardo <epardser@myuax.com>
2023-06-01 14:49:53 +0200

    .

--- index.py
+++ index.py
@@ -1,4 +1,9 @@
 
-if __name__ == 'main':
-    print('Lo primero que tienes que hacer es copiar y pegar lo siguiete en la consola de Python:')
-    print('python -m pip install requirements.txt')
+
+print('--Lo primero que hay que hace es activar el entorno virtual de Python. Para ello, en la consola de Python, escribe lo siguiente:')
+print('./venv/Scripts/activate')
+print('--Ahora lo segundo que tienes que hacer es copiar y pegar lo siguiete en la consola de Python:')
+print('python -m pip install requirements.txt')
+
+if __name__ == 'index':
+    pass
